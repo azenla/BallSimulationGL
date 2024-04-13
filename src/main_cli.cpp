52ed09aea207a4b7073d958f1516eca778ace19f
a dinosaur <dino@a-dinosaur.com>
2024-04-13 21:50:24 +1000

    fix cli & place shared code into library

--- src/main_cli.cpp
+++ src/main_cli.cpp
@@ -1,20 +1,20 @@
-#include "simulator.h"
+#include "simulator.hpp"
+#include "ball.hpp"
+#include "world.hpp"
 
 #include <thread>
 #include <chrono>
 
 int main() {
-    auto world = new BallSimulator::World(1024, 1024);
+    auto world = std::make_unique<BallSimulator::World>(1024, 1024);
     for (auto i = 1; i <= 20; i++) {
         auto ball = new BallSimulator::Ball(5.0f, 20.0f);
         world->add(ball);
     }
 
     for (auto i = 1; i <= 1000000; i++) {
-        world->tick(0.01f);
+        BallSimulator::DoQuadtreeCollisionDetection(*world, 0.01f);
     }
-
-    delete world;
 
     return 0;
 }
